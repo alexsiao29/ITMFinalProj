d37ca42b55365ea1556881afc83c88c7d06e5fcf
alexsiao29 <68944886+alexsiao29@users.noreply.github.com>
2023-07-22 20:14:34 +0800

    Delete admueats directory

--- admueats/admueats/store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Tables
-from .models import Occupancy
-
-# Register your models here.
-
-admin.site.register(Tables)
-admin.site.register(Occupancy)
--- admueats/admueats/store/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Occupancy 
-
-class Occupancy(ModelForm):
-    class Meta:
-        model=Occupancy,
-        fields=('____all___',)
